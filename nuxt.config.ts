525d65d3f6ae9af4d225d99a03c61ef2d0f41490
corner <corner3499@gmail.com>
2022-12-20 16:55:14 +0900

    tailwind css install setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,53 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+    // https://v3.nuxtjs.org/guide/directory-structure/nuxt.config/
+
+    // As of RC12 Nuxt 3 supports Hybrid rendering mode
+    // https://v3.nuxtjs.org/guide/concepts/rendering#route-rules
+    //   routeRules: {
+    //     '/pages/**': { swr: true },
+    //     '/posts/**': { static: true },
+    //   },
+
+    css: [
+        '@/assets/styles/tailwind.css',
+    ],
+
+    modules: [
+        '@nuxtjs/tailwindcss',
+        // https://pinia.esm.dev
+        // https://vueuse.org/
+    ],
+
+    runtimeConfig: {
+        public: {
+            directusUrl: process.env.DIRECTUS_URL,
+        },
+    },
+
+    postcss: {
+        plugins: {
+            'postcss-import': {},
+            'tailwindcss/nesting': {},
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+
+    //   Currently still needed
+    build: {
+        // transpile: ['@heroicons/vue', '@headlessui/vue'],
+    },
+
+    /*vite: {
+        optimizeDeps: {
+            include: [
+                '@heroicons/vue/20/solid',
+                '@heroicons/vue/24/solid',
+                '@heroicons/vue/24/outline',
+                '@headlessui/vue',
+                'vue',
+                'pinia',
+            ],
+        },
+    },*/
+})
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,14 @@
 <template>
     <div>
-        <h1>Index</h1>
+        <h1 class='hover:bg-black hover:text-white text-8xl'>Index</h1>
+        <span class="badge text-sm">Test</span>
     </div>
 </template>
 
-<script>
-export default {
-    name: "index"
-}
-</script>
+<script lang="ts"></script>
 
-<style scoped>
-
+<style lang="postcss" scoped>
+    .badge {
+        @apply bg-blue-500 text-white font-bold rounded-full px-3 py-1;
+    }
 </style>
